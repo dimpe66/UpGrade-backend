5152f01dc8015d4af5f669ed86ccc026027e43a2
FacuAlonso <112171600+FacuAlonso@users.noreply.github.com>
2025-11-02 01:06:29 -0300

    cors

--- express/src/index.ts
+++ express/src/index.ts
@@ -4,12 +4,14 @@ import userRoutes from "./routes/user.routes";
 import subjectRoutes from "./routes/subject.routes";
 import availabilityRoutes from "./routes/availability.routes";
 import lessonRoutes from "./routes/lesson.routes";
+import cors from "cors";
 
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT;
 
+app.use(cors());
 app.use(express.json());
 
 app.use("/users", userRoutes);
